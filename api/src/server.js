21330956f521970ac8de1e7001469cc03e13c1ac
usifu <usifubarriem@gmail.com>
2026-02-11 14:23:15 +0000

    Second Commit

--- api/src/server.js
+++ api/src/server.js
@@ -1,71 +1,20 @@
-import express from "express";
-import path from "path";
-import { clerkMiddleware } from "@clerk/express";
-import { serve } from "inngest/express";
-import cors from "cors";
-
-import { functions, inngest } from "./config/inngest.js";
-
-import { ENV } from "./config/env.js";
-import { connectDB } from "./config/db.js";
-
-import adminRoutes from "./routes/admin.route.js";
-import userRoutes from "./routes/user.route.js";
-import orderRoutes from "./routes/order.route.js";
-import reviewRoutes from "./routes/review.route.js";
-import productRoutes from "./routes/product.route.js";
-import cartRoutes from "./routes/cart.route.js";
-import paymentRoutes from "./routes/payment.route.js";
+import express from 'express';
+import path from 'path';
+import { ENV } from './config/env.js';
 
 const app = express();
 
 const __dirname = path.resolve();
 
-// special handling: Stripe webhook needs raw body BEFORE any body parsing middleware
-// apply raw body parser conditionally only to webhook endpoint
-app.use(
-  "/api/payment",
-  (req, res, next) => {
-    if (req.originalUrl === "/api/payment/webhook") {
-      express.raw({ type: "application/json" })(req, res, next);
-    } else {
-      express.json()(req, res, next); // parse json for non-webhook routes
-    }
-  },
-  paymentRoutes
-);
-
-app.use(express.json());
-app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
-
-app.use("/api/inngest", serve({ client: inngest, functions }));
-
-app.use("/api/admin", adminRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/reviews", reviewRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/cart", cartRoutes);
-
-app.get("/api/health", (req, res) => {
-  res.status(200).json({ message: "Success" });
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ message: "API is healthy!" });
 });
 
-// make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../admin/dist")));
-
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
-  });
+if(ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../admin/dist')));
+  app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../admin/dist/index.html'));
+  })
 }
 
-const startServer = async () => {
-  await connectDB();
-  app.listen(ENV.PORT, () => {
-    console.log("Server is up and running");
-  });
-};
-
-startServer();
+app.listen(ENV.PORT, () =>  console.log('API server is running'));
