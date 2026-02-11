7d216b774c29ceceab0fb4eb93a093dd2d29a419
usifu <usifubarriem@gmail.com>
2026-02-11 20:43:05 +0000

    file modified

--- api/src/server.js
+++ api/src/server.js
@@ -1,20 +1,30 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { ENV } from './config/env.js';
 
 const app = express();
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const projectRoot = path.resolve(__dirname, '..'); // points to the api folder
 
 app.get('/api/health', (req, res) => {
-  res.status(200).json({ message: "API is healthy!" });
+  res.status(200).json({ message: 'API is healthy!' });
 });
 
-if(ENV.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../admin/dist')));
-  app.get('/{*any}', (req, res) => {
-    res.sendFile(path.join(__dirname, '../admin/dist/index.html'));
-  })
+if (ENV.NODE_ENV === 'production') {
+  const adminDistPath = path.resolve(projectRoot, '../admin/dist');
+
+  app.use(express.static(adminDistPath));
+
+  app.get('*', (req, res, next) => {
+    if (req.path.startsWith('/api')) {
+      return next();
+    }
+
+    res.sendFile(path.join(adminDistPath, 'index.html'));
+  });
 }
 
-app.listen(ENV.PORT, () =>  console.log('API server is running'));
+app.listen(ENV.PORT, () => console.log('API server is running'));
